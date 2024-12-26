5e3f46d3dbcc0bb0937d691faca3b6e90aad4af2
pishty1 <pishty1@gmail.com>
2024-12-26 23:57:22 +0000

    update

--- src/sketches/repo/tut5.cljs
+++ src/sketches/repo/tut5.cljs
@@ -48,55 +48,139 @@
     :wrap (wrap location w h)
     (wrap location w h)))
 
+(defn create-fire-particle [location]
+  {:location location
+   :velocity [(* (q/random-gaussian) 1.2)
+              (- (* (q/random-gaussian) 1.4) 1.0)]
+   :acceleration [0 0]
+   :lifespan 255
+   :mass 10
+   :size (+ 10 (rand-int 20))})
+
+(defn dead? [{:keys [lifespan]}]
+  (< lifespan 0.0))
+
+(defn update-fire-particle [{:keys [acceleration velocity location lifespan] :as particle}]
+  (let [velocity (v/add velocity acceleration)]
+    (assoc particle
+           :velocity velocity
+           :location (v/add velocity location)
+           :lifespan (- lifespan 4.0)
+           :acceleration [0 0])))
+
+(defn display-fire-particle [{:keys [lifespan size] [x y] :location}]
+  (q/color-mode :rgb)
+  (q/fill (q/color 255 24 0 lifespan))
+  (q/no-stroke)
+  (let [particle-size (q/map-range lifespan 0 255 0 size)]
+    (q/ellipse x y particle-size particle-size)))
+
 (defn setup []
   (q/smooth)
   (q/background 230 230 230)
   (q/stroke 130, 0 0)
   (q/stroke-weight 4)
   (let [number-of-balls 100]
-    {:balls (map (fn [_]
-                   {:location [(rand-int (q/width)) (rand-int (q/height))]
-                    :speed [(rand-int 2) (rand-int 2)]
-                    :acc [0 0]
-                    :stroke (list 0 0 100)
-                    :fill  (list  0 0 255)
-                    :mass (q/floor (+ 1 (rand-int 10)))})
-                 (range number-of-balls))}))
+    {:black-ball {:location [(/ (q/width) 2) (/ (q/height) 2)]
+                  :speed [0 0]
+                  :acc [0 0]
+                  :stroke (list 0 0 0)
+                  :fill (list 0 0 0)
+                  :mass 15}
+     :balls (map (fn [_]
+                  {:location [(rand-int (q/width)) (rand-int (q/height))]
+                   :speed [(rand-int 2) (rand-int 2)]
+                   :acc [0 0]
+                   :stroke (list 0 0 100)
+                   :fill  (list  0 0 255)
+                   :mass (q/floor (+ 1 (rand-int 10)))
+                   :max-speed (+ 5 (rand-int 15))
+                   :on-fire false})
+                (range number-of-balls))
+     :fire-particles []}))
 
-(defn update-state [{:keys [balls] :as state}]
-  (loop [myballs balls
-         newballs []]
-    (if (seq myballs)
-      (let [ball (first myballs)
-            location    (:location ball)
-            speed       (:speed ball)
-            mass       (:mass ball)
-            direction (v/sub [(q/mouse-x) (q/mouse-y)] location)
-            org-distance (v/mag direction)
-            distance (if (< org-distance 5)
-                       5
-                       (if (> org-distance 25)
-                         25
-                         org-distance))
-            magnitude (/ (* 1 mass 20) (* distance distance))
-            norm-direction (v/norm direction)
-            scaled-direction (v/mult norm-direction magnitude)
-            updated-speed (v/add speed scaled-direction)
-            limited-speed (v/limit updated-speed 10)
-            updated-location (v/add location limited-speed)]
-        (recur (rest myballs)
-               (conj newballs (assoc ball
-                                     :location updated-location
-                                     :speed limited-speed
-                                     :acc [0 0]
-                                     :fill (list (* 10 org-distance) org-distance 255)
-                                     :stroke (list (* 2 org-distance) (* 1 org-distance) 100)
-                                     ))))
-      (assoc state
-             :balls newballs))))
+(defn check-collision [ball black-ball]
+  (let [distance (v/mag (v/sub (:location ball) (:location black-ball)))]
+    (< distance 33))) ; Sum of radii (26/2 + 40/2)
 
-(defn draw-state [{:keys [balls]}]
+(defn create-explosion [location]
+  (repeatedly 40 #(create-fire-particle location)))
+
+(defn update-state [{:keys [balls black-ball fire-particles] :as state}]
+  (let [; First update black ball position based on mouse
+        black-location (:location black-ball)
+        black-speed (:speed black-ball)
+        black-mass (:mass black-ball)
+        mouse-direction (v/sub [(q/mouse-x) (q/mouse-y)] black-location)
+        mouse-distance (max 5 (min 25 (v/mag mouse-direction)))
+        mouse-magnitude (/ (* 1 black-mass 20) (* mouse-distance mouse-distance))
+        mouse-force (v/mult (v/norm mouse-direction) mouse-magnitude)
+        updated-black-speed (v/limit (v/add black-speed mouse-force) 10)
+        updated-black-location (v/add black-location updated-black-speed)
+        updated-black-ball (assoc black-ball 
+                                :location updated-black-location
+                                :speed updated-black-speed)
+        
+        ; Update fire particles
+        updated-fire-particles (->> fire-particles
+                                  (map update-fire-particle)
+                                  (remove dead?))
+        
+        ; Then update all other balls and check for collisions
+        [updated-balls new-explosions] 
+        (loop [myballs balls
+               newballs []
+               explosions []]
+          (if (seq myballs)
+            (let [ball (first myballs)
+                  location (:location ball)
+                  speed (:speed ball)
+                  mass (:mass ball)
+                  max-speed (:max-speed ball)
+                  ; Check collision with black ball
+                  colliding? (check-collision ball updated-black-ball)
+                  ; If colliding and not already on fire, create explosion
+                  new-explosions (if (and colliding? (not (:on-fire ball)))
+                                 (concat explosions (create-explosion location))
+                                 explosions)
+                  ; Mouse attraction
+                  mouse-dir (v/sub [(q/mouse-x) (q/mouse-y)] location)
+                  org-distance (v/mag mouse-dir)
+                  distance (max 5 (min 25 org-distance))
+                  magnitude (/ (* 1 mass 20) (* distance distance))
+                  norm-direction (v/norm mouse-dir)
+                  mouse-force (v/mult norm-direction magnitude)
+                  ; Black ball repulsion
+                  black-dir (v/sub location updated-black-location)
+                  black-distance (max 5 (v/mag black-dir))
+                  black-magnitude (/ (* 2 mass black-mass 40) (* black-distance black-distance))
+                  black-force (v/mult (v/norm black-dir) black-magnitude)
+                  ; Combine forces
+                  total-force (v/add mouse-force black-force)
+                  updated-speed (v/add speed total-force)
+                  limited-speed (v/limit updated-speed max-speed)
+                  updated-location (v/add location limited-speed)]
+              (recur (rest myballs)
+                     (if colliding?
+                       newballs  ; Remove colliding balls
+                       (conj newballs (assoc ball
+                                           :location updated-location
+                                           :speed limited-speed
+                                           :acc [0 0]
+                                           :on-fire colliding?
+                                           :fill (list (* 10 org-distance) org-distance 255)
+                                           :stroke (list (* 2 org-distance) (* 1 org-distance) 100))))
+                     new-explosions))
+            [newballs explosions]))]
+    (assoc state
+           :black-ball updated-black-ball
+           :balls updated-balls
+           :fire-particles (concat updated-fire-particles new-explosions))))
+
+(defn draw-state [{:keys [balls black-ball fire-particles]}]
   (q/background 190 130 30)
+  
+  ; Draw regular balls
   (q/stroke-weight 5)
   (doseq [ball balls]
     (q/stroke (first (:stroke ball))
@@ -106,15 +190,28 @@
             (second (:fill ball))
             (last (:fill ball)))
     (q/ellipse (first (:location ball)) (second (:location ball)) 26 26))
-  (let [center [(/ (q/width) 2)
-                (/ (q/height) 2)]
-        mouse [(q/mouse-x)
-               (q/mouse-y)]
-        diff (v/sub  mouse center)
-        ;; scaled-diff (v/mult diff 0.8)
-        scaled-diff-div (v/div diff 1)
-        mag (v/mag scaled-diff-div)
-        [finalx finaly] scaled-diff-div]))
+  
+  ; Draw fire particles
+  (doseq [particle fire-particles]
+    (display-fire-particle particle))
+  
+  ; Draw black ball
+  (q/stroke (first (:stroke black-ball))
+            (second (:stroke black-ball))
+            (last (:stroke black-ball)))
+  (q/fill (first (:fill black-ball))
+          (second (:fill black-ball))
+          (last (:fill black-ball)))
+  (q/ellipse (first (:location black-ball)) 
+             (second (:location black-ball)) 
+             40 40)
+  
+  ; Draw ball count
+  (q/fill 255)
+  (q/stroke 0)
+  (q/stroke-weight 2)
+  (q/text-size 24)
+  (q/text (str "Balls: " (count balls)) 20 40))
 
 (defn start []
   (q/defsketch Something2
